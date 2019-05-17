34b3ede1e15ddd6b18e74853471d5b64aea1f238
Gabor Gerencser <kispeka94@gmail.com>
2019-05-17 14:13:04 +0200

    Person Class

---
+++ src/coupletask/Person.java
@@ -0,0 +1,32 @@
+package coupletask;
+
+/**
+ *
+ * @author Gerencsér Gábor
+ */
+public class Person {
+    
+    private String name;
+    private String city;
+    private String email;
+
+    public Person(String name, String city, String email) {
+        this.name = name;
+        this.city = city;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    
+}

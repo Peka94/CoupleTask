5ffb01a73a01c8621a7c52a3c72162757a70b0b9
Gabor Gerencser <kispeka94@gmail.com>
2019-05-17 15:02:08 +0200

    comparator

--- src/coupletask/CoupleTask.java
+++ src/coupletask/CoupleTask.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package coupletask;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  *
@@ -15,7 +13,12 @@ public class CoupleTask {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
     }
     
+    public static void sortPersons (){
+        ArrayList<Person> persons = Reader.read();
+        persons.getName(new NameComparator());
+    }
 }
